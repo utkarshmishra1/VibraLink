440bea0f7115a22492636e329d800cd93fd20eae
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-09-03 15:20:52 +0530

    Initial Commit

---
+++ VibraLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VibraLink
+//
+//  Created by utkarsh mishra on 03/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VibraLink/VibraLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibraLinkApp.swift
+//  VibraLink
+//
+//  Created by utkarsh mishra on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct VibraLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
